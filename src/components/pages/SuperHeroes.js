1bf44151a6264257a716be7da52379884126c1e8
md.farid <md.farid.ice@gmail.com>
2023-01-28 10:14:09 +0600

    Error handling using traditional and react-query

--- src/components/pages/SuperHeroes.js
+++ src/components/pages/SuperHeroes.js
@@ -2,18 +2,29 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 function SuperHeroes() {
+  const [error, setError] = useState('');
   const [heroes, setHeroes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes').then((res) => {
-      setHeroes(res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get('http://localhost:4000/superheroes')
+      .then((res) => {
+        setHeroes(res.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
   }, []);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>{error}</h2>;
   }
 
   return (
