9627c8242a9846ba46bce2a89b7e926ee33a3a66
md.farid <md.farid.ice@gmail.com>
2023-01-28 11:43:15 +0600

    networking calling properties values

--- src/components/pages/RQSuperHeroes.js
+++ src/components/pages/RQSuperHeroes.js
@@ -11,7 +11,14 @@ function RQSuperHeroes() {
     'super-heroes',
     fetchSuperHeroes,
     {
+      /** When isLoading true or false that's means network call is happened forground or not */
+      //cacheTime: 30000,
+      /** When isFetching true or false that's means network call is happened background or not */
       staleTime: 30000,
+      /** network call is happended or not while on mount */
+      //refetchOnMount: true
+      /** network call is happended or not while on windows focus */
+      //refetchOnWindowFocus: true
     }
   );
 
