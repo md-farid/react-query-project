44995b7f943ce04a9a7891e83326c324687e4524
md.farid <md.farid.ice@gmail.com>
2023-01-28 08:59:22 +0600

    Traditional style to fetch data in react

--- src/components/pages/SuperHeroes.js
+++ src/components/pages/SuperHeroes.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function SuperHeroes() {
-  return <div>SuperHeroes</div>;
+  const [heroes, setHeroes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/superheroes').then((res) => {
+      setHeroes(res.data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading ...</h2>;
+  }
+
+  return (
+    <div>
+      <h2>Super Heroes</h2>
+      {heroes.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>;
+      })}
+    </div>
+  );
 }
 
 export default SuperHeroes;
