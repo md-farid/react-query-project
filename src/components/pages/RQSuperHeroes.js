80a52b5d11c315179ee58ae874c728656fe1b1fe
md.farid <md.farid.ice@gmail.com>
2023-01-28 11:48:50 +0600

    polling data in time interval

--- src/components/pages/RQSuperHeroes.js
+++ src/components/pages/RQSuperHeroes.js
@@ -14,11 +14,14 @@ function RQSuperHeroes() {
       /** When isLoading true or false that's means network call is happened forground or not */
       //cacheTime: 30000,
       /** When isFetching true or false that's means network call is happened background or not */
-      staleTime: 30000,
+      //staleTime: 30000,
       /** network call is happended or not while on mount */
       //refetchOnMount: true
       /** network call is happended or not while on windows focus */
       //refetchOnWindowFocus: true
+      /** polling data in spefice time interval */
+      refetchInterval: 2000,
+      refetchIntervalInBackground: true,
     }
   );
 
